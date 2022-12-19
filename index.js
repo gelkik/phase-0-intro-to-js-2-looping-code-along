5f2f0afc46982ca125ab33f71df8c7a4e7bc9170
George Elkik <gelkik@gmail.com>
2022-12-18 20:35:51 -0500

    commit tree 82b8e153baea62a69746c524d754f55b4f022664

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const greet = []
+
+function writeCards(name){
+    for (let i = 0; i < name.length; i++){
+        greet[i] = `Thank you, ${name[i]}, for the wonderful surprise gift!`;
+    }
+    return greet;    
+}
+
+
+function countDown(number){
+    for (let i = number; i > -1; i--){
+        console.log(i);
+    }
+}
