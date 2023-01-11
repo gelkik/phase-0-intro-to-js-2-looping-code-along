ab535c000bb5f78b16c60533095bf4e26dd10037
George Elkik <gelkik@gmail.com>
2023-01-11 17:44:22 -0500

    final

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const writeCards = e => {
+    const newArray = [];
+    for (let i in e){
+        newArray[i] = `Thank you, ${e[i]}, for the wonderful surprise gift!`
+    }
+    return newArray;
+}
+
+const countDown = e => {
+    while (e > -1){
+        console.log(e--);
+    }
+}
